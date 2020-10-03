4bce8034ad22a1a1127757ce406fbf152b32e19d
osnat ben ari <osnat122ster@gmail.com>
2020-10-04 01:07:05 +0300

    commit

--- components/MatrixBox.js
+++ components/MatrixBox.js
@@ -1,29 +1,26 @@
-
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const MatrixBox = (props) => {
-   const [ currentValue, setCurrentValue ] = useState(null)
+  const [currentValue, setCurrentValue] = useState(null);
 
-   const onBoxClick = (e) => {
-    // console.log(e.target)
-    if(currentValue === null && !props.winner) {
-        console.log(props.winner)
-        setCurrentValue(props.player)  
-        const key = props.id
-        props.onClick(currentValue, key)
+  const onBoxClick = (e) => {
+    if (currentValue === null && !props.winner) {
+      setCurrentValue(props.player);
+      const key = props.id;
+      props.onClick(currentValue, key);
     }
-   
-   }
+  };
 
-    return ( 
-        <div className="box"
-            value= {currentValue}
-            onClick={onBoxClick}
-            key={`${props.id}`}>
-                {currentValue}
-        </div> 
-    );
-    
-}
- 
+  return (
+    <div
+      className="box"
+      value={currentValue}
+      onClick={onBoxClick}
+      key={`${props.id}`}
+    >
+      {currentValue}
+    </div>
+  );
+};
+
 export default MatrixBox;
